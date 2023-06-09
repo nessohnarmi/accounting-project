0cab0764741225e7bea4932aab47c252f412c364
Your Name <durjoydey663@gmail.com>
2023-06-09 09:43:05 -0700

    durjoy

--- Product_add.php
+++ Product_add.php
@@ -29,9 +29,14 @@ $result = $db->query($sql);
         <div class="row">
             <div class="col-md-12">
                 <form action="product_insert.php" method="POST">
-                <p>Product Name : <input name="name" type="text" class="form-control" placeholder="Enter your name"></p>
-                <p>Description : <input name="description" type="text" class="form-control" placeholder="Enter User desription"></p>
-                
+                   
+                    <p>Product Name : <input name="name" type="text" class="form-control" placeholder="Enter your name"></p>
+                    <!-- <p><b>Image :</b>  <input type="file" value=""  name="image" id="image"  class="form-control" placeholder="Enter Image"/></p> -->
+                    <p>Product Id : <input name="product_id" type="text" class="form-control" placeholder="Enter User desription"></p>
+                    <p>Buy Rate : <input name="buy_rate" type="text" class="form-control" placeholder="Enter User desription"></p>
+                    <p>Initial Quantity : <input name="initial_quantity" type="text" class="form-control" placeholder="Enter User desription"></p>
+                    <p>Description : <input name="description" type="text" class="form-control" placeholder="Enter User desription"></p>
+                   
                
                 <button type="submit" class="btn btn-success">Submit</button>
                 <button type="reset" class="btn btn-danger">Reset</button>
--- Product_edit.php
+++ Product_edit.php
@@ -13,18 +13,16 @@
 
 <!-- Page wrapper  -->
 <!-- ============================================================== -->
-<div class="page-wrapper">
+<div class="app-content">
     <!-- ============================================================== -->
     <!-- Bread crumb and right sidebar toggle -->
     <!-- ============================================================== -->
-    <div class="page-breadcrumb bg-white">
-        <div class="row align-items-center">
-            <div class="col-md-12">
-                <h2 class="page-title">Edit Page</h2>
+    <div class="container">
+        <div class="row">
+            <div class="col-md-10">
+                <h2> Products Edit Page</h2>  <hr>              
             </div>
         </div>
-        <!-- /.col-lg-12 -->
-    </div>
     <!-- ============================================================== -->
     <!-- End Bread crumb and right sidebar toggle -->
     <!-- ============================================================== -->
@@ -41,6 +39,10 @@
                     <form action="update-Product.php" method="POST">
                             <input type="hidden" name="id" value="<?php echo $id; ?>">
                         <p>Product Name : <input name="name" type="text" value="<?php echo $data['name'] ; ?>" class="form-control" placeholder="Enter your name"></p>
+                        <!-- <p>Image : <input type="file"  name="image" id="image"  value="<?php echo $data['image'] ; ?>" class="form-control" ></p> -->
+                        <p>Product Id  : <input name="product_id" type="text" value="<?php echo $data['product_id'] ; ?>" class="form-control" placeholder="Enter User product id "></p>
+                        <p>Buy Rate  : <input name="buy_rate" type="text" value="<?php echo $data['buy_rate'] ; ?>" class="form-control" placeholder="Enter User buy rate "></p>
+                        <p>Initial Quantity  : <input name="initial_quantity" type="text" value="<?php echo $data['initial_quantity'] ; ?>" class="form-control" placeholder="Enter User initial quantity "></p>
                         <p>Description  : <input name="description" type="text" value="<?php echo $data['description'] ; ?>" class="form-control" placeholder="Enter User description "></p>
                 
                         <button type="submit" class="btn btn-success">Submit</button>
